323b572cab44605c01e6f6324668ce7259abc227
JaredVidelefsky <jared.videlefsky@gmail.com>
2023-04-05 18:18:22 -0400

    Take image from UI and some bug fixes

--- streamlit/1_ClothesReviewHub.py
+++ streamlit/1_ClothesReviewHub.py
@@ -12,7 +12,7 @@ aws_access_key_id = config('aws_access_key_id')
 aws_secret_access_key = config('aws_secret_access_key')
 # log_aws_access_key_id = config('log_aws_access_key_id')
 # log_aws_secret_access_key = config('log_aws_secret_access_key')
-# Google Credentials
+# Google Credentials 
 credentials = service_account.Credentials.from_service_account_file('damg7245-team3-assignment5-b0ba9f72c8a3.json')
 
 # S3 Details:
@@ -25,7 +25,9 @@ s3_client = boto3.client(
     aws_secret_access_key=aws_secret_access_key,
 )
 
-
+##########################################################################
+# Goog Cloud Vision APIs
+# Detect Labels
 def detect_labels(file):
     """Detects labels in the file."""
     client = vision.ImageAnnotatorClient(credentials=credentials)
@@ -33,12 +35,15 @@ def detect_labels(file):
     # with io.open(file, 'rb') as image_file:
     #     content = image_file.read()
     content = file.read()
-
+    
     image = vision.Image(content=content)
-
+    # st.write(type(image))
     response = client.label_detection(image=image)
     labels = response.label_annotations
     print('Labels:')
+
+    # Close opened file
+    file.close()
 
     label_list = []
     for label in labels:
@@ -52,7 +57,38 @@ def detect_labels(file):
                 response.error.message))
     
     return label_list
-    
+
+# Detect Logos
+def detect_logos(file):
+    """Detects logos in the file."""
+    client = vision.ImageAnnotatorClient(credentials=credentials)
+
+    # with io.open(file, 'rb') as image_file:
+    #     content = image_file.read()
+    content = file.read()
+
+    image = vision.Image(content=content)
+    # st.write(type(image))
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    # Close opened file
+    file.close()
+
+    logo_list = []
+    for logo in logos:
+        logo_list.append(logo.description)
+        print(logo.description)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+    return logo_list
+
 
 #########################################################################
 # Create a dropdown to select an existing file from the S3 bucket
@@ -68,70 +104,81 @@ selected_file = st.selectbox("Select an existing file from S3", ["None"] + s3_fi
 # TODO: max size is 4MB
 uploaded_file = st.file_uploader("Choose an image (jpg):", type="jpg")
 
-if uploaded_file is not None:
-    # Upload to S3
-    if uploaded_file.name.split('.')[1] not in ('jpg'):
-        st.error(f'Incorrect File Format!')
-        st.stop()
-    # Get the file name and size
-    filename = uploaded_file.name
-    # if not filename == st.session_state.file_name:
-    #     st.session_state.transcription_generated = False
-    #     st.session_state.transcript = ''
-    #     st.session_state.file_name = filename
-    #     st.session_state.convert_to_transcript = False
-    filesize = uploaded_file.size
+# Check if both an uploaded file and an S3 file were selected
+if uploaded_file and selected_file != "None":
+    st.write("Error: please select only one option")
+else:
+    # Check if an uploaded file was selected
+    if uploaded_file is not None:
+        # Upload to S3
+        if uploaded_file.name.split('.')[1] not in ('jpg'):
+            st.error(f'Incorrect File Format!')
+            st.stop()
+        # Get the file name and size
+        filename = uploaded_file.name
+        # if not filename == st.session_state.file_name:
+        #     st.session_state.transcription_generated = False
+        #     st.session_state.transcript = ''
+        #     st.session_state.file_name = filename
+        #     st.session_state.convert_to_transcript = False
+        filesize = uploaded_file.size
 
-    # Print the file name and size
-    st.write("File name:", filename)
-    st.write("File size:", filesize, "bytes")
+        # Print the file name and size
+        st.write("File name:", filename)
+        st.write("File size:", filesize, "bytes")
 
-    # Upload the file to the S3 bucket if it doesn't already exist
-    if filename not in s3_files:
-        s3_client.upload_fileobj(uploaded_file, s3_bucket_name, 'Input/' + filename)
-        st.write("File uploaded successfully!")
-    else:
-        st.write("File already exists in S3 bucket.")
-    
-    # Load image
-    image = Image.open(uploaded_file)
-    # TESTING
-    # print(uploaded_file)
-    # print(type(uploaded_file))
-    # print(type(image))
+        # Upload the file to the S3 bucket if it doesn't already exist
+        if filename not in s3_files:
+            s3_client.upload_fileobj(uploaded_file, s3_bucket_name, 'Input/' + filename)
+            st.write("File uploaded successfully!")
+        else:
+            st.write("File already exists in S3 bucket.")
+        
+        # Load image
+        image = Image.open(uploaded_file)
+        # TESTING
+        # print(uploaded_file) 
+        # print(type(uploaded_file))
+        # print(type(image))
 
-    # Display image
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+        # Display image
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Get Image file from S3
-    response = s3_client.get_object(Bucket=s3_bucket_name, Key='Input/' + filename)
-    image_data = response["Body"]
+        # Get Image file from S3
+        response = s3_client.get_object(Bucket=s3_bucket_name, Key='Input/' + filename)
+        image_data = response["Body"]
 
-    # Google Vision API to detect labels
-    label_list = detect_labels(image_data)
+        # Detect labels
+        label_list = detect_labels(image_data)
 
-    st.write(f'Label List: {label_list}')
+        st.write(f'Label List: {label_list}')
 
-    # """Detects labels in the file."""
-    # client = vision.ImageAnnotatorClient(credentials=credentials)
+        # Detect labels
+        logo_list = detect_logos(image_data)
 
-    # # TESTING 
-    # path = 'Example_Images/patagonia_fleece.jpg'
+        st.write(f'Logo List: {logo_list}')
 
-    # with io.open(path, 'rb') as image_file:
-    #     content = image_file.read()
 
-    # image = vision.Image(content=content)
+        # """Detects labels in the file."""
+        # client = vision.ImageAnnotatorClient(credentials=credentials)
 
-    # response = client.label_detection(image=image)
-    # labels = response.label_annotations
-    # print('Labels:')
+        # # TESTING 
+        # path = 'Example_Images/patagonia_fleece.jpg'
 
-    # for label in labels:
-    #     print(label.description)
+        # with io.open(path, 'rb') as image_file:
+        #     content = image_file.read()
 
-    # if response.error.message:
-    #     raise Exception(
-    #         '{}\nFor more info on error messages, check: '
-    #         'https://cloud.google.com/apis/design/errors'.format(
-    #             response.error.message))
+        # image = vision.Image(content=content)
+
+        # response = client.label_detection(image=image)
+        # labels = response.label_annotations
+        # print('Labels:')
+
+        # for label in labels:
+        #     print(label.description)
+
+        # if response.error.message:
+        #     raise Exception(
+        #         '{}\nFor more info on error messages, check: '
+        #         'https://cloud.google.com/apis/design/errors'.format(
+        #             response.error.message))
---
+++ streamlit/pages/2_Camera_Image.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import boto3
+from datetime import datetime
+from decouple import config
+
+# AWS KEYS
+aws_access_key_id = config('aws_access_key_id')
+aws_secret_access_key = config('aws_secret_access_key')
+# log_aws_access_key_id = config('log_aws_access_key_id')
+# log_aws_secret_access_key = config('log_aws_secret_access_key')
+
+# S3 Details:
+s3_bucket_name = 'damg7245-assignment5'
+
+# Create an S3 client
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+)
+
+#######################################################
+st.title('Image from Camera')
+
+s3_files_2 = []
+
+image_title = st.text_input('Image Title', '')
+picture = st.camera_input("Take a picture:")
+
+if picture and image_title != '':
+    st.image(picture)
+    filename = image_title + '_' + datetime.now().strftime("%d_%m_%y") + '.jpg'
+
+    # Upload the file to the S3 bucket if it doesn't already exist
+    if filename not in s3_files_2:
+        # with open (filename,'wb') as file:
+        #     file.write(picture.getbuffer())
+        s3_client.upload_fileobj(picture, s3_bucket_name, 'Input/' + filename)
+        st.write("File uploaded successfully!")
+    else:
+        st.write("File already exists in S3 bucket.")
+    
+    st.write(f'{filename} uploaded to S3! Go to ClothesReviewHub and select file from S3 dropdown.')
+    # TODO: Auto selected in select an existing file from S3 on the ClothesReviewHub page
+    
