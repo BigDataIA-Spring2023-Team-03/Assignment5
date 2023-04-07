c35cc5b17589defe49566711faae3bdc69e4e653
manideepakreddyaila <aila.m@northeastern.edu>
2023-04-07 17:41:26 -0400

    changes

--- streamlit/ClothesReviewHub.py
+++ streamlit/ClothesReviewHub.py
@@ -78,7 +78,7 @@ st.title('ClothesReviewHub')
 st.subheader('Three Upload Options: Take a Picture, Select Existing from S3, Upload an Image')
 
 # Take Picture Option
-link = 'http://52.202.105.109:8501/Camera_Image'
+link = 'http://44.214.57.19:8501/Camera_Image'
 
 st.markdown(f'To take a picture using your device, use this link: <a href={link}>Take Picture</a>', unsafe_allow_html=True)
 
@@ -105,18 +105,14 @@ else:
     else:
         st.write("No File Selected")
 
-# TODO: max size is 4MB
 uploaded_file = st.file_uploader("Choose an image (jpg):", type="jpg")
 
-# Check if both an uploaded file and an S3 file were selected
 if uploaded_file and selected_file != "None":
     st.write("Error: please select only one option")
 
 else:
 
     if selected_image is not None:
-        # st.image(selected_image, caption="Uploaded Image", use_column_width=True)
-        # st.write(selected_file)
         response = s3_client.get_object(Bucket=s3_bucket_name, Key='Input/' + selected_file)
         image_data = response["Body"]
         label_list, logo_list = detect_labels_logos(image_data)
@@ -132,15 +128,11 @@ else:
         st.write(label_query)
         st.write(f'Logo List: {logo_list}')
 
-        # Search for Similar Products
         st.subheader('Select Marketplace to Search for Similar Products:')
         walmart = st.checkbox('Get Walmart Reviews')
-        # amazon = st.checkbox('Amazon - (Deprecated)')
 
         if walmart:
-            # TODO
             products, reviews = WalmartReviews(label_query)
-            # st.write(reviews)
             log_results = {'Image_File': selected_file,
                            'Logo_List': logo_list,
                            'Label_List': label_list,
@@ -151,18 +143,13 @@ else:
             for j,i in enumerate(products):
                 st.image(i, width=300)
 
-                # Learn More
                 st.write('Want to Learn More? Check below to generate a summary of product reviews:')
                 learn_more = st.checkbox('Learn More?', key = j)
-                # Generate Summary of Reviews
                 if learn_more:
                     all_reviews = ""
-                    # TODO: Generate Summary of reviews
                     st.title('Summary of Reviews')
                     for item in reviews:
-                        # check if the 'title' field exists in the current item
                         st.text("")
-                        # st.write(item)
                         summary_prompt = "Summarize the below reviews into 2-3 sentences and suggest whether a user should buy the product:"
 
                         completion = openai.ChatCompletion.create(
@@ -173,60 +160,30 @@ else:
 
                         chat_output = completion.choices[0].message.content.strip()
                         st.write(chat_output)
-                        # if 'title' in item:
-                        #     st.subheader(item['title'])
-                        # # display the 'review' field
-                        # st.write(item['review'])
-                        # all_reviews += item['review']
-                    #
-                    # shoud_buy = st.checkbox('Should I buy this product?')
-                    # if shoud_buy:
-                    #     buy_decision = should_buy_product(all_reviews)
-                    #     st.write(buy_decision)
-    # Check if an uploaded file was selected
+
     if uploaded_file is not None:
         # Upload to S3
         if uploaded_file.name.split('.')[1] not in ('jpg'):
             st.error(f'Incorrect File Format!')
             st.stop()
-        # Get the file name and size
         filename = uploaded_file.name
-        # filename = obj.get('ResponseMetadata').get('HTTPHeaders').get('x-amz-meta-filename')
-        # filesize = obj.get('ResponseMetadata').get('HTTPHeaders').get('ContentLength')
-
-        # if not filename == st.session_state.file_name:
-        #     st.session_state.transcription_generated = False
-        #     st.session_state.transcript = ''
-        #     st.session_state.file_name = filename
-        #     st.session_state.convert_to_transcript = False
         filesize = uploaded_file.size
 
-        # Print the file name and size
         st.write("File name:", filename)
         st.write("File size:", filesize, "bytes")
 
-        # Upload the file to the S3 bucket if it doesn't already exist
         if filename not in s3_files:
             s3_client.upload_fileobj(uploaded_file, s3_bucket_name, 'Input/' + filename)
             st.write("File uploaded successfully!")
         else:
             st.write("File already exists in S3 bucket.")
-        
-        # Load image
-        image = Image.open(uploaded_file)
-        # TESTING
-        # print(uploaded_file) 
-        # print(type(uploaded_file))
-        # print(type(image))
 
-        # Display image
+        image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
         # Get Image file from S3
         response = s3_client.get_object(Bucket=s3_bucket_name, Key='Input/' + filename)
         image_data = response["Body"]
-
-        # Detect labels and Logos
         label_list, logo_list = detect_labels_logos(image_data)
 
         st.write(f'Label List: {label_list}')
@@ -240,16 +197,12 @@ else:
         st.write(label_query)
         st.write(f'Logo List: {logo_list}')
 
-        # Search for Similar Products
         st.subheader('Select Marketplace to Search for Similar Products:')
         walmart = st.checkbox('Get Walmart Reviews')
-        # amazon = st.checkbox('Amazon - (Deprecated)')
 
         if walmart:
-            # TODO
             products, reviews = WalmartReviews(label_query)
 
-            # AWS CloudWatch Logging - TODO
             log_results = {'Image_File': filename,
                            'Logo_List': logo_list,
                            'Label_List': label_list,
@@ -259,7 +212,6 @@ else:
             write_logs(str(log_results))
 
             json_str = json.dumps(log_results)
-            # Upload the JSON file to S3
             s3_client.put_object(Bucket=s3_bucket_name, Key='Results/' + filename.split('.')[0] + '.json',
                                  Body=json_str)
 
@@ -267,25 +219,14 @@ else:
 
             for j,i in enumerate(products):
                 st.image(i, width=300)
-
-
-
-            # Upload JSON Results file to S3
-            # Convert the dictionary to JSON
-
-
-            # Learn More
                 st.write('Want to Learn More? Check below to generate a summary of product reviews:')
                 learn_more = st.checkbox('Learn More?', key = j)
                 # Generate Summary of Reviews
                 if learn_more:
                     all_reviews = ""
-                    # TODO: Generate Summary of reviews
                     st.title('Summary of Reviews')
                     for item in reviews:
-                        # check if the 'title' field exists in the current item
                         st.text("")
-                        # st.write(item)
                         summary_prompt = "Summarize the below reviews into 2-3 sentences and suggest whether a user should buy the product:"
 
                         completion = openai.ChatCompletion.create(
